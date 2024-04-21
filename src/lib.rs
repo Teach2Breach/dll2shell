962cf4c6fe8642a3c205c476d1fd802e91b863dc
Kirk Trychel <105792760+Teach2Breach@users.noreply.github.com>
2024-04-21 16:31:40 -0500

    Add files via upload

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+use std::fs::File;
+use std::io::Write;
+mod shellcode;
+
+    pub fn gen_shellcode (args: Vec<String>) {   // create shellcode and output to file
+
+        //println!("args: {:?}", args);
+        let link_shellcode = shellcode::shellcode_rdi(&args[1], &args[2], "".to_string());
+        let mut output_file = File::create("shellcode.bin").expect("could not write file");
+        output_file
+            .write_all(&link_shellcode)
+            .expect("could not write contents to output file");
+        println!("output: shellcode.bin");
+    }
