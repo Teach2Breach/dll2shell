370f117508bf87f852bb485bae3f0632d8dc149a
Teach2Breach <ktrychel@gmail.com>
2025-01-19 10:31:57 -0600

    cleanup and working on a bug

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use std::fs::File;
-use std::io::Write;
 pub mod shellcode;
+use dll2shell::gen_shellcode;
+
 
 fn main() {
-
     //collect command line args
     let args: Vec<String> = std::env::args().collect();
 
@@ -13,17 +12,5 @@ fn main() {
         return;
     }
     
-    fn gen_shellcode (args: Vec<String>) {   // create shellcode and output to file
-
-        //println!("args: {:?}", args);
-        let link_shellcode = shellcode::shellcode_rdi(&args[1], &args[2], "".to_string());
-        let mut output_file = File::create("shellcode.bin").expect("could not write file");
-        output_file
-            .write_all(&link_shellcode)
-            .expect("could not write contents to output file");
-        println!("output: shellcode.bin");
-    }
-
     gen_shellcode(args);
-
 }
